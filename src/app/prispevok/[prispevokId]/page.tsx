d7200bd62135db15d0afcbef86b69b8cc30329ec
addadadsdeefef <adam.tesak99@gmail.com>
2024-10-04 09:29:56 +0200

    syntax updated

--- src/app/prispevok/[prispevokId]/page.tsx
+++ src/app/prispevok/[prispevokId]/page.tsx
@@ -4,11 +4,14 @@ import Typography from '@mui/material/Typography';
 
 export const metadata = { title: "Detail príspevku | ZoškaSnap" };
 
-export default function PostDetail() {
-
+export default function PostDetail({ 
+  params, 
+}: {
+  params: { prispevokId:string };
+}) {
   return (
 
-      <Typography> Detail príspevku </Typography>
+      <Typography> Detail príspevku {params.prispevokId} </Typography>
 
   );
 }
